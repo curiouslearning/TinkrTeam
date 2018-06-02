56726f53f52a8df03b5810f0f564809af8337d2c
Gurunatraj <p.gurunatraj@gmail.com>
2018-06-02 11:42:17 +0530

    cat story json fill
    Signed-off-by: Gurunatraj <p.gurunatraj@gmail.com>

---
+++ TinkrShelf/Assets/Books/CatStory/Resources/CatSManager9.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatSManager9 : GSManager {
+//
+//	// Use this for initialization
+//	void Start () {
+//		
+//	}
+//	
+//	// Update is called once per frame
+//	void Update () {
+//		
+//	}
+	public override void OnMouseDown(GameObject go)
+	{
+		if (go.name == "Jam") 
+		{  // StartCoroutine (SetGameObject());
+			GameObject seqAnim = GameObject.Find ("CatJam");
+			GTinkerGraphic tinkerGraphic1 = seqAnim.GetComponent<GTinkerGraphic>();
+			if (tinkerGraphic1 != null)
+			{
+				tinkerGraphic1.MyOnMouseDown();
+
+			}
+		}
+
+
+	}
+//	public IEnumerator SetGameObject ()
+//	{
+//		yield return new WaitForSeconds (2.0f);
+//		GameObject.Find ("Jam").SetActive (false);
+//	}
+
+}
