eb2ba6a92df94a6cc285afd3d8815e24b093406a
thebetalegend <shubhamjain2796@gmail.com>
2018-07-01 13:51:08 +0530

    level 5 audio done
    Signed-off-by: thebetalegend <shubhamjain2796@gmail.com>

---
+++ TinkrShelf/Assets/Books/catstorylevel5/Resources/L4CatSManager9.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L4CatSManager9 : GSManager {
+
+	GameObject jam;
+
+	public override void OnMouseDown(GameObject go)
+	{
+		if (go.name == "CatJam") {
+			if (jam == null) {
+				jam = GameObject.Find ("Jam");
+			}
+
+			jam.SetActive (true);
+		}
+		else if ( go.name == "Text_jam."){
+
+			if (jam == null) {
+				jam = GameObject.Find ("Jam");
+			}
+
+			jam.SetActive (true);
+			GTinkerGraphic catHam = GameObject.Find("CatJam").GetComponent<GTinkerGraphic>();
+			if (catHam != null)
+			{ 
+				catHam.MyOnMouseDown();
+			}
+
+		}
+		else if (go.name == "Jam") 
+		{   
+			jam = go;
+			GTinkerGraphic catJam = GameObject.Find("CatJam").GetComponent<GTinkerGraphic>();
+			if (catJam != null)
+			{ 
+				catJam.MyOnMouseDown();
+			}
+		}
+
+		base.OnMouseDown (go);
+	}
+
+
+}
