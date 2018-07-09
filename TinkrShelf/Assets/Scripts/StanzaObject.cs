c9657782dc1a19f487bbcc8a300af1a4bacd562f
thebetalegend <shubhamjain2796@gmail.com>
2018-07-09 13:59:50 +0530

    audio echo removed
    Signed-off-by: thebetalegend <shubhamjain2796@gmail.com>

--- TinkrShelf/Assets/Scripts/StanzaObject.cs
+++ TinkrShelf/Assets/Scripts/StanzaObject.cs
@@ -9,7 +9,7 @@ public class StanzaObject : MonoBehaviour {
 	public TextClass stanzaValue;
 	public List<GTinkerText> tinkerTexts;// The minimum spacing between words
 	public GStanzaManager stanzaManager;
-	public bool stanzaNarrate;
+	//public bool stanzaNarrate;
 
 	// Time delay at end of stanza during autoplay
 	public float endDelay;
@@ -26,8 +26,8 @@ public class StanzaObject : MonoBehaviour {
 	/// zoomin and zoomout of texts with delays acoordinf to the start and end time of texts 
 	/// </summary>
 	/// <param name="startingTinkerText">Starting tinker text.</param>
-	public IEnumerator AutoPlay(GTinkerText startingTinkerText = null)
-	{   stanzaNarrate = true;
+	public IEnumerator AutoPlay(GTinkerText startingTinkerText = null) { 
+	//{   stanzaNarrate = true;
 		int startingTinkerTextIndex = 0;
 
 		if (startingTinkerText != null)
@@ -42,15 +42,15 @@ public class StanzaObject : MonoBehaviour {
             GTinkerText t = null;
             if (tinkerTexts[i]!=null)
              t = tinkerTexts[i];
-			GTinkerGraphic link = t.GetComponent<GTinkerText> ().pairedGraphic;
+			//GTinkerGraphic link = t.GetComponent<GTinkerText> ().pairedGraphic;
 			//if paired graphic is present
-			if (link != null) {
-				GSManager scenescript = stanzaManager.sceneManager;
+			//if (link != null) {
+				//GSManager scenescript = stanzaManager.sceneManager;
 				//call its respective scene onmousedown
-				scenescript.OnMouseDown(t.gameObject);
-				scenescript.OnMouseUp (t.gameObject);
+				//scenescript.OnMouseDown(t.gameObject);
+				//scenescript.OnMouseUp (t.gameObject);
 
-			}
+			//}
 
    
 			Animator anim = t.GetComponent<Animator>();
@@ -105,7 +105,7 @@ public class StanzaObject : MonoBehaviour {
 			}
 
 		}
-		stanzaNarrate=false;
+		//stanzaNarrate=false;
 
 		// Stop the coroutine
 		yield break;
@@ -148,7 +148,7 @@ public class StanzaObject : MonoBehaviour {
 		mouseDownTinkerText = tinkerText;
 
 		// And trigger the tinkerText only if stanza narrate is false 
-		if(stanzaNarrate==false)
+		//if(stanzaNarrate==false)
 		tinkerText.OnPairedMouseDown();
 	}
     /// <summary>
