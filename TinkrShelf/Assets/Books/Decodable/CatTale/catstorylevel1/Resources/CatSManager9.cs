202d99ce9fb1b76425b196008456a3e4c8ccf811
Gurunatraj <p.gurunatraj@gmail.com>
2018-08-15 22:33:18 +0530

    working book
    Signed-off-by: Gurunatraj <p.gurunatraj@gmail.com>

--- TinkrShelf/Assets/Books/Decodable/CatTale/catstorylevel1/Resources/CatSManager9.cs
+++ TinkrShelf/Assets/Books/Decodable/CatTale/catstorylevel1/Resources/CatSManager9.cs
@@ -3,41 +3,38 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CatSManager9 : GSManager {
-
 	GameObject jam;
+
+	public override void Start ()
+	{   jam = GameObject.Find ("Jam");
+
+		base.Start ();
+	}
 
 	public override void OnMouseDown(GameObject go)
 	{
-		if (go.name == "CatJam") {
-			if (jam == null) {
-				jam = GameObject.Find ("Jam");
-			}
+		if (go.name == "CatJam" || go.name=="Text_cat's") {
 
+			jam.SetActive(true);
+		} else if (go.name == "Text_jam") {
 			jam.SetActive (true);
-		}
-		else if ( go.name == "Text_jam"){
+			GTinkerGraphic catJam = GameObject.Find ("CatJam").GetComponent<GTinkerGraphic> ();
+			if (catJam != null) {
+				//catJam.reset();
+				catJam.MyOnMouseDown ();
 
-			if (jam == null) {
-				jam = GameObject.Find ("Jam");
-			}
-
-			jam.SetActive (true);
-			GTinkerGraphic catHam = GameObject.Find("CatJam").GetComponent<GTinkerGraphic>();
-			if (catHam != null)
-			{ 
-				catHam.MyOnMouseDown();
 			}
 
 		}
-		else if (go.name == "Jam") 
-		{  
-			jam = go;
-			GTinkerGraphic catHam = GameObject.Find("CatJam").GetComponent<GTinkerGraphic>();
-			if (catHam != null)
-			{ 
-				catHam.MyOnMouseDown();
-			}
-		}
+		//		else if (go.name == "Jam") 
+		//		{   
+		//			jam = go;
+		//			GTinkerGraphic catJam = GameObject.Find("CatJam").GetComponent<GTinkerGraphic>();
+		//			if (catJam != null)
+		//			{ 
+		//				catJam.MyOnMouseDown();
+		//			}
+		//		}
 
 		base.OnMouseDown (go);
 	}
